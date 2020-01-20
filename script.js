f799567ede20f237e5720a9e0a715b4c8f114ea7
AlpixTM <contact@alpix.eu>
2020-01-20 06:49:01 +0100

    :art: Reformat code: js in extra file

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    run();
+});
+
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function run() {
+    var colId = 1;
+    var bar = $(".bar");
+    var i = 1001;
+
+    while(true) {
+        // Remove old progress class, so you can add the new class in the next step
+        bar.removeClass("bar-" + i);
+
+        //if its at the end reset it
+        //else just let it grow further
+        if (i > 1000) {
+            await Sleep(500);
+            $("." + colId).prop('checked', true);
+            colId += 1;
+            if (colId > 3) {
+                colId = 1;
+            }
+            i = 0;
+            bar.removeClass("bar-0");
+            bar.addClass("bar-" + (i));
+            await Sleep(500);
+        }
+        else {
+            // Make the progress appear random
+            i += Math.floor(Math.random() * Math.floor(Math.random() * 25));
+            bar.addClass("bar-" + (i));
+            await Sleep(50 + Math.floor(Math.random() * + Math.floor(Math.random() * 10) * Math.random() * Math.floor(Math.random() * 10) * Math.random() * Math.floor(Math.random() * 10) * Math.random()));
+        }
+    }
+}
