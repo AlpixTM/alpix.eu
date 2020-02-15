41678b2f28791e52ca3d8bfd6ce5cacfbd1abaff
AlpixTM <contact@alpix.eu>
2020-02-15 21:45:33 +0100

    :lipstick: Elements falling down

--- matix/js/matix.js
+++ matix/js/matix.js
@@ -2,13 +2,20 @@ $(document).ready(function () {
     run();
 });
 
-function run() {
-    $("#body").append("<div id='1'>x</div>");
-    $("#body").append("<div id='2'>y</div>");
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function run() {
+    $(".site").append("<div id='1' class='test'>x</div>");
+    $(".site").append("<div id='2' class='test'>y</div>");
 
 
 
-    for (let j = 0; j < 900; j++) {
-        $("#1").css("margin-top", j/10 + "%");
+    for (let j = 0; j < 1000; j++) {
+        $("#1").css("top", j/10 + "%");
+        $("#2").css("top", j/10 + "%");
+        await Sleep(10);
     }
+
 }
